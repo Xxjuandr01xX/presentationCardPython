33eea1ab04cea1bd8985b88d4ad305d174ec40fe
Juan D. Rincon U <jd.rincon021@gmail.com>
2023-01-29 21:31:59 -0400

    pagina principal y declaracion de session administrador

--- index.py
+++ index.py
@@ -14,7 +14,15 @@ mysql = MySQL(app)
 
 @app.route("/")
 def home():
-    return render_template('index_profile.html')
+    ##Pagina principal 
+    cursor = mysql.connection.cursor()
+    cursor.execute("SELECT * FROM py_profile_person WHERE id=1")
+    data = cursor.fetchall()
+    return render_template('home.html', perfil=data)
 
+@app.route("/administrador")
+def admin():
+    ## Pagina para inicio de Session en Administrador
+    pass
 if __name__ == "__main__":
     app.run(port=3000, debug=True)
